5279ce57f001b705cd31fe9c9584eb364a3c2795
Little Jacob <joelnoumbi28@gmail.com>
2023-02-19 17:40:04 +0100

    projects overflow-x

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -17,17 +17,16 @@ const Projects = ({ projects }: Props) => {
         Projects
       </h3>
 
-      <div className="sm:mt-0 mt-10 relative flex overflow-x-scroll  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="w-full sm:mt-0 mt-10 relative flex overflow-x-scroll scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin overflow-y-hidden snap-x snap-mandatory z-20">
         {projects?.map((project, i) => (
-          <motion.div
-            key={project?._id}
-            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          <Link
+            href={project?.linkToBuild}
+            target="_blank"
+            rel="noopener"
           >
-            <Link
-              href={project?.linkToBuild}
-              target="_blank"
-              rel="noopener"
-              className="transform transition duration-500 hover:scale-150"
+            <motion.div
+              key={project?._id}
+              className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
             >
               <motion.img
                 initial={{
@@ -39,37 +38,37 @@ const Projects = ({ projects }: Props) => {
                 transition={{ duration: 1.2 }}
                 src={urlFor(project?.projectImage).url()}
                 alt={project?.title}
-                className="cursor-pointer w-[100px] h-fit object-cover object-center"
+                className="hidden sm:flex cursor-pointer sm:w-[100px] w-14 h-fit object-cover object-center transform transition hover:scale-150"
               />
-            </Link>
 
-            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-xl sm:text-4xl font-semibold text-center">
-                <span className="underline decoration-[#F7AB0A]/50">
-                  Case study {i + 1} of {projects.length}:
-                </span>{" "}
-                {project?.title}
-              </h4>
-            </div>
+              <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                <h4 className="text-xl sm:text-4xl font-semibold text-center">
+                  <span className="underline decoration-[#F7AB0A]/50">
+                    Case study {i + 1} of {projects.length}:
+                  </span>{" "}
+                  {project?.title}
+                </h4>
+              </div>
 
-            <div className="flex items-center space-x-2 justify-center">
-              {project?.technologies?.map((tech) => (
-                <Image
-                  key={tech?._id}
-                  src={urlFor(tech?.skillImage).url()}
-                  width={40}
-                  height={40}
-                  alt={tech?.title}
-                  className="object-cover w-auto h-auto"
-                  priority
-                />
-              ))}
-            </div>
+              <div className="flex items-center space-x-2 justify-center">
+                {project?.technologies?.map((tech) => (
+                  <Image
+                    key={tech?._id}
+                    src={urlFor(tech?.skillImage).url()}
+                    width={20}
+                    height={20}
+                    alt={tech?.title}
+                    className="object-cover w-auto h-auto"
+                    priority
+                  />
+                ))}
+              </div>
 
-            <p className="sm:text-lg text-sm text-center md:text-left">
-              {project?.summary}
-            </p>
-          </motion.div>
+              <p className="sm:text-lg text-sm text-center md:text-left">
+                {project?.summary}
+              </p>
+            </motion.div>
+          </Link>
         ))}
       </div>
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
