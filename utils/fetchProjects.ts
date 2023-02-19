f6b0ed85c21e7744338a0290377e02b0756cd9dd
Little Jacob <joelnoumbi28@gmail.com>
2023-02-19 11:30:55 +0100

    removed!!! clg

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -4,7 +4,5 @@ export const fetchProjects = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
   const data = await res.json();
   const projects: Project[] = data?.project;
-
-  console.log("fetching", projects);
   return projects;
 };
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -4,7 +4,5 @@ export const fetchSocials = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
   const data = await res.json();
   const socials: Social[] = data?.socials;
-
-  console.log("fetching", socials);
   return socials;
 };
